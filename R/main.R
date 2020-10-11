f6b7d36a2f68b8786cf7076a64ce8826c35983ef
Mick Mioduszewski <mick@mioduszewski.net>
2020-10-12 07:05:57 +1100

    ..

--- R/main.R
+++ R/main.R
@@ -1,23 +1,62 @@
 rf <- new.env(parent = emptyenv())
 
-rf$start_time    <- function() Sys.time()
-rf$tz            <- function() Sys.timezone()
-
 #' @title
-#' Get `framerrr` framework start time
+#' Start the framework
 #'
 #' @description
-#' `get_start_time` returns an object of class "POSIXct" that contains time
-#'   when `framerrr` was loaded
+#' Sets defaults and starts the framework
 #'
 #' @details
 #' This is function has no parameters.
 #'
-#' @return The time the package was loaded.
+#' @param project A character vector of length 1 containing the name of the
+#'   project.
+#' @param dref A character vector with the list of
+#'   configuration entries that contain multiple definitions,
+#'   each referring to a specific named operating environment.
+#' @param tz A time zone to use in date and time calculations.
+#' @param log A name of a variable that contains the definition of the log
+#'   location for the project.
+#' @param global_config A definition of the location of the global
+#'   configuration file
+#' @param local_config A definition of the location of the local
+#'   configuration file
+#' @param f_log A definition of the log
+#'   location for the project.
+#' @param d_output A definition of the output
+#'   directory.
+#' @param d_input A definition of the input
+#'   directory.
+#' @param d_internal A definition of the internal
+#'   directory
+#' @return The configuration environment.
 #' @export
 #'
 #' @examples
-#' get_start_time()
-get_start_time   <- function() {
-  rf$start_time
+#' run_start()
+run_start <-
+  function(project       = "project",
+           dref          = "",
+           tz            = Sys.timezone(),
+           log           = c("f_log"),
+           global_config = file.path(dirname(getwd()), "config", "config.R"),
+           local_config  = file.path(getwd(), "config.R"),
+           f_log         = list(file = "log.csv", dir = "d_internal"),
+           d_output      = c(dir = file.path(dirname(dirname(getwd())),
+                                             "AnalyticSoftwareOutput")),
+           d_input       = c(dir = file.path(dirname(dirname(getwd())),
+                                             "AnalyticSoftwareInput")),
+           d_internal    = c(dir = file.path(dirname(dirname(getwd())),
+                                             "AnalyticSoftwareInternal"))) {
+    rf$project       <- project
+    rf$dref          <- dref
+    rf$tz            <- Sys.timezone()
+    rf$log           <- log
+    rf$global_config <- global_config
+    rf$local_config  <- local_config
+    rf$f_log         <- f_log
+    rf$d_output      <- d_output
+    rf$d_input       <- d_input
+    rf$d_internal    <- d_internal
+    return(invisible(rf))
   }
